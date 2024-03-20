c0af59480124cba100f0faaf57f9253ae0f02cd9
Rachel Strohecker <159338038+r-stro@users.noreply.github.com>
2024-03-19 22:48:22 -0400

    eerk

--- teamfil.py
+++ teamfil.py
@@ -4,22 +4,22 @@ import matplotlib.animation as animation
 
 
 class Obstacle:
-    def __init__(self, position, radius):
-        self.position = position  # Position of the obstacle (center)
+    def __init__(self, pos, radius):
+        self.pos = pos  # pos of the obstacle (center)
         self.radius = radius  # Radius of the obstacle
 
     def is_colliding(self, point):
-        distance = np.linalg.norm(point - self.position)
-        return distance <= self.radius
+        dist = np.linalg.norm(point - self.pos)
+        return dist <= self.radius
 
 
 class UAV:
-    def __init__(self, position, fov, targ_pos, obstacles=[], alpha=1, beta=1):
-        self.position = position
-        self.velocity = np.zeros(2)
+    def __init__(self, pos, fov, targ_pos, obstacles=[], alpha=1, beta=1):
+        self.pos = pos
+        self.vel = np.zeros(2)
         self.color = 'blue'
         self.no_hit = True
-        self.best_pos = self.position.copy()
+        self.best_pos = self.pos.copy()
         self.best_score = float('inf')
         self.fov = fov
         self.targ_pos = targ_pos
@@ -30,20 +30,20 @@ class UAV:
         self.beta = beta    # ACO parameter beta
         self.pheromone_map = np.zeros(8)
 
-    def update_position(self, homebase, plot_area, step_size):
-        distance = np.linalg.norm(self.position - homebase)
+    def update_pos(self, homebase, plot_area, step_size):
+        dist = np.linalg.norm(self.pos - homebase)
         self.is_targ_within_fov()
 
         if not self.at_targ:
-            if (distance < plot_area) and (self.no_hit == True):
-                direction = (self.position - homebase) / distance
-                self.velocity = direction * step_size
-                self.position += self.velocity
+            if (dist < plot_area) and (self.no_hit == True):
+                direction = (self.pos - homebase) / dist
+                self.vel = direction * step_size
+                self.pos += self.vel
 
-
-            elif (distance == plot_area) and (self.no_hit == True):
-                self.color = 'red'
+            elif (dist == plot_area) and (self.no_hit == True):
+                self.color = 'black'
                 self.no_hit = False
+
             elif not self.at_targ:  
                 self.pso()
                 self.color = 'red'
@@ -56,92 +56,92 @@ class UAV:
                     self.pso()  
             # Check for obstacle collisions and avoid them
         for obstacle in self.obstacles:
-                if obstacle.is_colliding(self.position):
+                if obstacle.is_colliding(self.pos):
                     self.avoid_obstacle(obstacle)
 
     def pso(self):
         inertia_weight = 0.5  # Inertia weight
         c_weight = 0.5  # Cognitive weight
         s_weight = 0.5  # Social weight
-        max_velocity = 0.50  # Maximum velocity
+        max_vel = 0.50  # Maximum vel
 
-        personal_best_position = self.best_pos
-        global_best_position = self.targ_pos
+        personal_best_pos = self.best_pos
+        global_best_pos = self.targ_pos
 
-        # Update velocity
-        c_comp = c_weight * np.random.rand() * (personal_best_position - self.position)
-        s_comp = s_weight * np.random.rand() * (global_best_position - self.position)
-        self.velocity = inertia_weight * self.velocity + c_comp + s_comp
+        # Update vel
+        c_comp = c_weight * np.random.rand() * (personal_best_pos - self.pos)
+        s_comp = s_weight * np.random.rand() * (global_best_pos - self.pos)
+        self.vel = inertia_weight * self.vel + c_comp + s_comp
 
-        self.velocity = np.clip(self.velocity, -max_velocity, max_velocity)
+        self.vel = np.clip(self.vel, -max_vel, max_vel)
 
-        self.position += self.velocity
+        self.pos += self.vel
 
-        score = np.linalg.norm(self.position - self.targ_pos)
+        score = np.linalg.norm(self.pos - self.targ_pos)
         if score < self.best_score:
-            self.best_pos = self.position.copy()
+            self.best_pos = self.pos.copy()
             self.best_score = score
 
     
-    def distance_to_target(self, direction):
-        next_position = self.position + self.move_in_direction(direction)
-        distance = np.linalg.norm(next_position - self.targ_pos)
-        return distance
+    def dist2target(self, direction):
+        next_pos = self.pos + self.move_in_direction(direction)
+        dist = np.linalg.norm(next_pos - self.targ_pos)
+        return dist
     
     def move_in_direction(self, direction):
         directions = np.array([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]])
-        movement_vectors = directions * np.linalg.norm(self.velocity)
+        movement_vectors = directions * np.linalg.norm(self.vel)
         return movement_vectors
     
 
     def check_fit(self):
-        score = np.linalg.norm(self.position - self.targ_pos)
+        score = np.linalg.norm(self.pos - self.targ_pos)
         return score
 
     def is_targ_within_fov(self):
-        dist_2_targ = np.linalg.norm(self.targ_pos - self.position)
-        if dist_2_targ < self.fov:
+        dist2targ = np.linalg.norm(self.targ_pos - self.pos)
+        if dist2targ < self.fov:
             self.at_targ = True
         else: 
             self.at_targ = False
-        return dist_2_targ < self.fov
+        return dist2targ < self.fov
 
     def is_uav_within_fov(self, other_uav):
-        dist_between_uavs = np.linalg.norm(self.position - other_uav.position)
+        dist_between_uavs = np.linalg.norm(self.pos - other_uav.pos)
 
-        avoidance_vector = self.position - other_uav.position
-        avoidance_vector /= np.linalg.norm(avoidance_vector)
+        avoid_vec = self.pos - other_uav.pos
+        avoid_vec /= np.linalg.norm(avoid_vec)
 
-        avoidance_distance = 2 * (3 + self.fov)  
-        avoidance_offset = avoidance_distance * avoidance_vector
-        self.position += avoidance_offset
+        avoid_dist = 2 * (3 + self.fov)  
+        avoid_offset = avoid_dist * avoid_vec
+        self.pos += avoid_offset
 
         return dist_between_uavs < self.fov * 2
     
     def avoid_obstacle(self, obstacle):
-        avoidance_vector = self.position - obstacle.position
-        avoidance_vector /= np.linalg.norm(avoidance_vector)
+        avoid_vec = self.pos - obstacle.pos
+        avoid_vec /= np.linalg.norm(avoid_vec)
 
-        avoidance_distance = 2 * obstacle.radius  
-        avoidance_offset = avoidance_distance * avoidance_vector
-        self.position += avoidance_offset
+        avoid_dist = 2 * (obstacle.radius + self.fov) 
+        avoid_offset = avoid_dist * avoid_vec
+        self.pos += avoid_offset
 
 class Swarm:
-    def __init__(self, num_uavs, homebase, plot_area, fov, targ_pos, num_obstacles=9):
-        self.num_uavs = num_uavs
+    def __init__(self, n_uavs, homebase, plot_area, fov, targ_pos, n_obstacles=9):
+        self.n_uavs = n_uavs
         self.homebase = homebase
         self.plot_area = plot_area
         self.fov = fov
         self.targ_pos = targ_pos
-        # Generate random obstacle positions
-        self.obstacles = [Obstacle(np.random.uniform(low=-10, high=10, size=2), 0.3) for _ in range(num_obstacles)]
-        self.uavs = [UAV(np.random.uniform(low=-1, high=1, size=2), self.fov, self.targ_pos, self.obstacles) for _ in range(num_uavs)]
+        # Generate random obstacle poss
+        self.obstacles = [Obstacle(np.random.uniform(low=-10, high=10, size=2), 0.3) for _ in range(n_obstacles)]
+        self.uavs = [UAV(np.random.uniform(low=-1, high=1, size=2), self.fov, self.targ_pos, self.obstacles) for _ in range(n_uavs)]
 
-    def update_positions(self, step_size):
+    def update_poss(self, step_size):
         target_detected = False  # Flag to track if the target is detected by any UAV
         for uav in self.uavs:
             if not uav.at_targ:
-                uav.update_position(self.homebase, self.plot_area, step_size)
+                uav.update_pos(self.homebase, self.plot_area, step_size)
             else:
                 target_detected = True
 
@@ -156,7 +156,7 @@ class Swarm:
             uav_best_score = uav.check_fit()
             if uav_best_score < uav.best_score:
                 uav.best_score = uav_best_score
-                uav.best_pos = uav.position
+                uav.best_pos = uav.pos
 
     def get_global_best_solution(self):
         best_score = float('inf')
@@ -168,39 +168,38 @@ class Swarm:
         return best_pos, best_score
 
 # Parameters
-num_uavs = 10  # Number of UAVs
-num_steps = 100  # Number of simulation steps
+n_uavs = 10  # Number of UAVs
+n_steps = 100  # Number of simulation steps
 step_size = 0.1  # Step size for movement
 homebase = np.array([0, 0])  # Homebase location
 plot_area = 10  # Size of the plotted area
 fov = 3.0  # Field of view
-# targ_pos = np.array(np.random.uniform(-5, 5, 2))
-targ_pos = (-6, 5)
+targ_pos = np.array(np.random.uniform(-5, 5, 2))
 
-swarm = Swarm(num_uavs, homebase, plot_area, fov, targ_pos)
+swarm = Swarm(n_uavs, homebase, plot_area, fov, targ_pos)
 
-def update_positions(i):
+def update_poss(i):
     global swarm
 
-    swarm.update_positions(step_size)
+    swarm.update_poss(step_size)
     swarm.check_fit()
     best_pos, best_score = swarm.get_global_best_solution()
 
-    # Plot
+    
     plt.clf()
     plt.scatter(homebase[0], homebase[1], color='green', label='Homebase')
     plt.scatter(targ_pos[0], targ_pos[1], color='purple', label='Target')
 
-    # Plot UAVs
+    #Plot UAVs
     for uav in swarm.uavs:
-        plt.scatter(uav.position[0], uav.position[1], color=uav.color)
+        plt.scatter(uav.pos[0], uav.pos[1], color=uav.color)
 
-    # Plot outer edge
+    
     circle = plt.Circle((0, 0), plot_area, color='green', fill=False, linestyle='--', label='Outer Edge')
     plt.gca().add_artist(circle)
 
     for obstacle in swarm.obstacles:
-        circle = plt.Circle(obstacle.position, obstacle.radius, color='red', alpha=0.5)
+        circle = plt.Circle(obstacle.pos, obstacle.radius, color='red', alpha=0.5)
         plt.gca().add_artist(circle)
 
     plt.title('Swarm of UAVs Movement')
@@ -211,11 +210,11 @@ def update_positions(i):
     plt.legend()
 
     if not uav.no_hit:
-        # Plot best position found
+        
         plt.scatter(best_pos[0], best_pos[1], color='red')
 
 # Create animation
 fig = plt.figure()
-ani = animation.FuncAnimation(fig, update_positions, frames=num_steps, interval=100)
+ani = animation.FuncAnimation(fig, update_poss, frames=n_steps, interval=100)
 
 plt.show()
